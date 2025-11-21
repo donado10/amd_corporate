69023cdeffa09699fbbc4e3c1c3cb8c3b73d031a
donado10 <adamscoulibaly10@gmail.com>
2025-11-21 16:15:41 +0000

    update

--- src/app/(dashboard)/gestion/ressources/drivers/page.tsx
+++ src/app/(dashboard)/gestion/ressources/drivers/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div className="p-2">Chauffeurs</div>;
+  return (
+    <div className="p-8">
+      <span className="text-xl font-semibold text-[#001945]">Chauffeurs</span>
+    </div>
+  );
 };
 
 export default page;
