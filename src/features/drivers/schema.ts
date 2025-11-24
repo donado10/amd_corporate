bae029846fa087db3b1b95c0a0fe659295fcdfd2
donado10 <adamscoulibaly10@gmail.com>
2025-11-24 22:39:27 +0000

    update

---
+++ src/features/drivers/schema.ts
@@ -0,0 +1,33 @@
+import { EPermission } from "@/lib/enum";
+import { z } from "zod";
+
+export const driverSchema = z.object({
+  em_no: z.number(),
+  em_firstname: z.string().min(3, {
+    message: "Veuillez renseigner le prénom du chauffeur",
+  }),
+  em_lastname: z.string().min(3, {
+    message: "Veuillez renseigner le nom du chauffeur",
+  }),
+  em_birthday: z.string(),
+  em_nationality: z
+    .string()
+    .min(3, { message: "Veuillez renseigner la nationalité" }),
+  em_birthplace: z
+    .string()
+    .min(3, { message: "Veuillez renseigner le lieu de naissance" }),
+  em_address: z
+    .string()
+    .min(3, { message: "Veuillez renseigner l'adresse du chauffeur" }),
+  em_phonenumber: z
+    .string()
+    .min(9, { message: "Veuillez renseigner le numéro du chauffeur" })
+    .max(14),
+  em_emergencynumber: z
+    .string()
+    .min(9, { message: "Veuillez renseigner le numéro du chauffeur" })
+    .max(14),
+  em_email: z.email(),
+  em_type: z.number(),
+  em_addons: z.object(),
+});
---
+++ src/features/drivers/server/route.ts
@@ -0,0 +1,55 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { loginSchema, registerSchema } from "@/features/schema";
+import createAdminClient from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie";
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { AUTH_COOKIE } from "@/lib/constants";
+import { client } from "@/lib/db-pgsql";
+import { driverSchema } from "../schema";
+
+const app = new Hono()
+  .get("/", async (c) => {
+    const result = await client.query("SELECT * FROM f_employee");
+
+    return c.json({ result: result.rows });
+  })
+  .post("/", zValidator("json", driverSchema), async (c) => {
+    const values = await c.req.valid("json");
+
+    const result = await client.query(
+      "CALL public.create_employee ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
+      [
+        values.em_firstname,
+        values.em_lastname,
+        values.em_birthday,
+        values.em_nationality,
+        values.em_birthplace,
+        values.em_address,
+        values.em_phonenumber,
+        values.em_email,
+        values.em_emergencynumber,
+        values.em_type,
+        values.em_addons,
+      ]
+    );
+
+    console.log(result);
+
+    return c.json({ message: "chauffeur crée" });
+  })
+  .delete("/", zValidator("json", driverSchema), async (c) => {
+    const values = await c.req.valid("json");
+
+    const result = await client.query(
+      "DELETE FROM public.f_employee WHERE em_no=$1",
+      [values.em_no]
+    );
+
+    console.log(result);
+
+    return c.json({ message: "chauffeur supprimé" });
+  });
+
+export default app;
