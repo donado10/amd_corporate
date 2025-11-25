8ad72ad78bb8db69ecf3247ad69563745d1c5803
donado10 <adamscoulibaly10@gmail.com>
2025-11-25 12:46:18 +0000

    update

--- src/features/cars/schema.ts
+++ src/features/cars/schema.ts
@@ -2,32 +2,19 @@ import { EPermission } from "@/lib/enum";
 import { z } from "zod";
 
 export const carSchema = z.object({
-  em_no: z.number(),
-  em_firstname: z.string().min(3, {
-    message: "Veuillez renseigner le prénom du chauffeur",
-  }),
-  em_lastname: z.string().min(3, {
-    message: "Veuillez renseigner le nom du chauffeur",
-  }),
-  em_birthday: z.string(),
-  em_nationality: z
-    .string()
-    .min(3, { message: "Veuillez renseigner la nationalité" }),
-  em_birthplace: z
-    .string()
-    .min(3, { message: "Veuillez renseigner le lieu de naissance" }),
-  em_address: z
-    .string()
-    .min(3, { message: "Veuillez renseigner l'adresse du chauffeur" }),
-  em_phonenumber: z
-    .string()
-    .min(9, { message: "Veuillez renseigner le numéro du chauffeur" })
-    .max(14),
-  em_emergencynumber: z
-    .string()
-    .min(9, { message: "Veuillez renseigner le numéro du chauffeur" })
-    .max(14),
-  em_email: z.email(),
-  em_type: z.number(),
-  em_addons: z.object(),
+  car_no: z.number(),
+  car_fueltype: z.string(),
+  car_tankcapacity: z.string(),
+  car_mileage: z.string(),
+  car_chassisnumber: z.string(),
+  car_enginenumber: z.string(),
+  car_fiscalpower: z.string(),
+  car_payload: z.string(),
+  car_acquisitiondate: z.string(),
+  car_acquisitiontype: z.string(),
+  car_registrationnumber: z.string(),
+  car_circulationdate: z.string(),
+  car_acquisitionvalue: z.string(),
+  car_owner: z.string(),
+  car_addons: z.object(),
 });
--- src/features/cars/server/route.ts
+++ src/features/cars/server/route.ts
@@ -11,41 +11,44 @@ import { carSchema } from "../schema";
 
 const app = new Hono()
   .get("/", async (c) => {
-    const result = await client.query("SELECT * FROM f_employee");
+    const result = await client.query("SELECT * FROM f_cars");
 
     return c.json({ result: result.rows });
   })
   .post("/", zValidator("json", carSchema), async (c) => {
     const values = await c.req.valid("json");
 
-    const result = await client.query(
-      "CALL public.create_employee ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
+    await client.query(
+      "CALL public.create_car ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)",
       [
-        values.em_firstname,
-        values.em_lastname,
-        values.em_birthday,
-        values.em_nationality,
-        values.em_birthplace,
-        values.em_address,
-        values.em_phonenumber,
-        values.em_email,
-        values.em_emergencynumber,
-        values.em_type,
-        values.em_addons,
+        values.car_fueltype,
+        values.car_tankcapacity,
+        values.car_mileage,
+        values.car_chassisnumber,
+        values.car_enginenumber,
+        values.car_fiscalpower,
+        values.car_payload,
+        values.car_acquisitiondate,
+        values.car_acquisitiontype,
+        values.car_registrationnumber,
+        values.car_circulationdate,
+        values.car_acquisitionvalue,
+        values.car_owner,
+        values.car_addons,
       ]
     );
 
-    return c.json({ message: "chauffeur crée" });
+    return c.json({ message: "Véhicule crée" });
   })
   .delete("/", zValidator("json", carSchema), async (c) => {
     const values = await c.req.valid("json");
 
     const result = await client.query(
-      "DELETE FROM public.f_employee WHERE em_no=$1",
-      [values.em_no]
+      "DELETE FROM public.f_cars WHERE em_no=$1",
+      [values.car_no]
     );
 
-    return c.json({ message: "chauffeur supprimé" });
+    return c.json({ message: "véhicule supprimé" });
   });
 
 export default app;
